6e4066788b410fdcacfb22a695822a3e2ab958c8
div-anh0901 <anh.dev.2002@gmail.com>
2023-03-17 14:05:07 +0700

    done list hotel

--- client/src/components/custom-slider_v1/index.tsx
+++ client/src/components/custom-slider_v1/index.tsx
@@ -2,44 +2,26 @@ import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
 import { PositionRelative, PositonAbsolute } from '../position/Position';
+import { Typography } from '@mui/material';
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+type Props ={
+  name: string;
+  type_hotel: string;
+  price: string;
+  images:string[]
+}
 
-const images = [
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-];
 
-function SwipeableTextMobileStepper() {
+
+const SwipeableTextMobileStepper:React.FC<Props> =(data)=> {
   const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
+  const maxSteps = data.images.length;
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -54,67 +36,95 @@ function SwipeableTextMobileStepper() {
   };
 
   return (
-    <PositionRelative>
-        <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-            <AutoPlaySwipeableViews
-              autoPlay={true}
+    
+        <Box sx={{ maxWidth: 300, flexGrow: 1 , border: "1px solid #ccc", marginBottom :"20px" }}>
+          <PositionRelative >
+            <SwipeableViews
+              loop={true}
+              autoPlay={false}
               axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
               index={activeStep}
               onChangeIndex={handleStepChange}
               enableMouseEvents
             >
-              {images.map((step, index) => (
-                <div key={step.label}>
+              {data.images.map((step, index) => (
+                <div key={index}>
                   {Math.abs(activeStep - index) <= 2 ? (
                     <Box
                       component="img"
                       sx={{
-                        height: 255,
+                        height: 200,
                         display: 'block',
-                        maxWidth: 400,
+                        maxWidth: 300,
                         overflow: 'hidden',
                         width: '100%',
                       }}
-                      src={step.imgPath}
-                      alt={step.label}
+                      src={step}
+                      alt=""
                     />
                   ) : null}
                 </div>
               ))}
-            </AutoPlaySwipeableViews>
+            </SwipeableViews>
+            
             <MobileStepper
-              steps={maxSteps}
+              steps={0}
               position="static"
+              variant="dots"
               activeStep={activeStep}
               nextButton={
-                  <PositonAbsolute top={0}>
+                  <PositonAbsolute top={0} right={0} >
                     <Button
                       size="small"
                       onClick={handleNext}
+                      sx={{height: "200px", display:"flex", alignItems: "center"}}
                       disabled={activeStep === maxSteps - 1}
                     >
-                      Next
-                      {theme.direction === 'rtl' ? (
-                        <KeyboardArrowLeft />
-                      ) : (
-                        <KeyboardArrowRight />
-                      )}
+                      <Box 
+                        sx={{height: "30px", width:"30px", borderRadius:"50%",backgroundColor:'white',color:"black",display:'flex', alignItems:"center",justifyContent:'center'}}
+                      >
+                        {theme.direction === 'rtl' ? (
+                          <KeyboardArrowLeft />
+                        ) : (
+                          <KeyboardArrowRight />
+                        )}
+                      </Box>
+                      
                     </Button>
                   </PositonAbsolute>
               }
               backButton={
-                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                  {theme.direction === 'rtl' ? (
-                    <KeyboardArrowRight />
-                  ) : (
-                    <KeyboardArrowLeft />
-                  )}
-                  Back
+                <PositonAbsolute top={0} left={0}  >
+                  <Button 
+                    size="small" 
+                    onClick={handleBack} 
+                    disabled={activeStep === 0}
+                    sx={{height: "200px", display:"flex", alignItems: "center", color:"white"}}  
+                  >
+                    <Box 
+                        sx={{height: "30px", width:"30px", borderRadius:"50%",backgroundColor:'white',color:"black",display:'flex', alignItems:"center",justifyContent:'center'}}
+                      >
+                        {theme.direction === 'rtl' ? (
+                          <KeyboardArrowRight />
+                        ) : (
+                          <KeyboardArrowLeft />
+                        )}
+                    </Box>
                 </Button>
+                </PositonAbsolute>
               }
             />
+            </PositionRelative>
+          <Typography variant='subtitle1' sx={{padding: "5px"}} >
+              {data.type_hotel}
+          </Typography>
+          <Typography variant='h5'  sx={{padding: "5px", width: "200px",whiteSpace:"nowrap",overflow:"hidden",textOverflow:"ellipsis"}} >
+            {data.name}
+          </Typography>
+          <Typography variant='subtitle1'sx={{padding: "5px"}} >
+              {data.price}
+          </Typography>
         </Box>
-    </PositionRelative>
     
   );
 }
--- client/src/section/dashboard/list-hotel/ListHotel.tsx
+++ client/src/section/dashboard/list-hotel/ListHotel.tsx
@@ -1,10 +1,23 @@
+import { Typography } from '@mui/material'
+import {Box} from '@mui/material'
+import Grid from '@mui/material/Grid'
 import React from 'react'
 import SwipeableTextMobileStepper from '../../../components/custom-slider_v1'
+import { listHotelData } from '../../../data/data'
 
 export default function ListHotel() {
   return (
     <>
-        <SwipeableTextMobileStepper/>
+        <Typography variant='h4' sx={{margin: "20px 0" }}> List Hotel</Typography>
+        <Grid container  spacing={2}>
+          {
+            listHotelData.map((data,index)=>(
+              <Grid xs={6} sm={4} md={3} lg={3} item>
+                  <SwipeableTextMobileStepper name={data.name} type_hotel={data.type_hotel}  price={data.price} images={data.images}/>
+              </Grid>
+            ))
+          }
+        </Grid>
     </>
   )
 }
